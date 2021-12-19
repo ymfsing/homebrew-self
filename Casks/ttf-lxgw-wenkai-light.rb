8d27310dd0b193c62e657310ba7d873dcca015c1
ymfsing <ymfsing@outlook.com>
2021-12-19 12:27:29 +0800

    add ttf-lxgw-wenkai all

--- Casks/ttf-lxgw-wenkai-light.rb
+++ Casks/ttf-lxgw-wenkai-light.rb
@@ -1,13 +1,13 @@
-cask "ttf-lxgw-wenkai-regular" do
+cask "ttf-lxgw-wenkai-light" do
   version "1.111"
   sha256 :no_check
 
-  url "https://github.com/lxgw/LxgwWenKai/releases/download/v#{version}/LXGWWenKai-Regular.ttf"
-  name "LXGW WenKai Regular"
+  url "https://github.com/lxgw/LxgwWenKai/releases/download/v#{version}/LXGWWenKai-Light.ttf"
+  name "LXGW WenKai Light"
   desc "An open-source Chinese font derived from Fontworks' Klee One."
   homepage "https://github.com/lxgw/LxgwWenKai"
 
-  font "TTF/LXGWWenKai-Regular.ttf"
+  font "TTF/LXGWWenKai-Light.ttf"
 
   caveats do
     free_license "https://github.com/lxgw/LxgwWenKai/raw/v#{version}/SIL_Open_Font_License_1.1.txt"
