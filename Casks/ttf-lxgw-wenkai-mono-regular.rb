d7f19381a635fdc2996f7c13d58c82dbe9f17a0f
ymfsing <ymfsing@outlook.com>
2021-12-19 12:47:23 +0800

    add lxgw-wenkai-mono

---
+++ Casks/ttf-lxgw-wenkai-mono-regular.rb
@@ -0,0 +1,15 @@
+cask "ttf-lxgw-wenkai-mono-regular" do
+  version "1.111"
+  sha256 :no_check
+
+  url "https://github.com/lxgw/LxgwWenKai/releases/download/v#{version}/LXGWWenKaiMono-Regular.ttf"
+  name "LXGW WenKai Mono Regular"
+  desc "An open-source Chinese font derived from Fontworks' Klee One."
+  homepage "https://github.com/lxgw/LxgwWenKai"
+
+  font "LXGWWenKaiMono-Regular.ttf"
+
+  caveats do
+    free_license "https://github.com/lxgw/LxgwWenKai/raw/v#{version}/SIL_Open_Font_License_1.1.txt"
+  end
+end
