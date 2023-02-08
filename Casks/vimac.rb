2370bc8302182ebb3f1137933b03110fec7c9487
ymfsing <ymfsing@outlook.com>
2023-02-09 04:43:57 +0800

    update vimac download url

--- Casks/vimac.rb
+++ Casks/vimac.rb
@@ -1,8 +1,8 @@
 cask "vimac" do
   version "0.3.19"
-  sha256 "a03ac25edca2190207c70825b154d20a3c6bc5b0d7b705d1ced95a7d0961c4a0"
+  sha256 :no_check
 
-  url "https://api.xuann.wang/api/app-center?owner=dexterleng&app=vimac&dist=sparkle&release=31"
+  url "https://github.com/ymfsing/files/raw/main/Vimac.app.zip"
   name "Vimac"
   desc "Keyboard-driven navigation and control tool"
   homepage "https://vimacapp.com/"
