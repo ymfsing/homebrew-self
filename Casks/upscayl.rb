176144bc8a98f3309ef291f40992b8704f41280c
ymfsing <ymfsing@outlook.com>
2023-04-18 10:02:33 +0800

    add upscayl

---
+++ Casks/upscayl.rb
@@ -0,0 +1,21 @@
+cask 'upscayl' do
+  version '2.0.1'
+  sha256 :no_check
+
+  url "https://github.com/upscayl/upscayl/releases/download/v#{version}/Upscayl-#{version}-mac.dmg"
+  name 'Upscayl'
+  desc 'Free and Open Source AI Image Upscaler for Linux, MacOS and Windows built with Linux-First philosophy.'
+  homepage 'https://github.com/upscayl/upscayl'
+
+  app 'Upscayl.app'
+
+  postflight do
+    set_permissions "#{appdir}/Upscayl.app/Contents/Resources/bin/upscayl", 'u+x'
+  end
+
+  zap trash: [
+        '~/Library/Application Support/Upscayl',
+        '~/Library/Preferences/org.upscayl.app.plist',
+        '~/Library/Saved Application State/org.upscayl.app.savedState'
+      ]
+end
