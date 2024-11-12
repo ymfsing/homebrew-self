8a9b376c732de593d1f92c7cb3ee0e7ee3f76254
ymfsing <ymfsing@outlook.com>
2024-11-12 16:11:52 +0800

    delete upscayl and update peazip cherry-studio

--- Casks/peazip.rb
+++ Casks/peazip.rb
@@ -1,9 +1,9 @@
 cask "peazip" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "9.9.1"
-  sha256 arm:   "82a3a7f1eba405f032c0a08d5e60a83c019bf8624fdd03020f75f4882de91bfe",
-         intel: "4266369de61adf72a21cbe9b36e79a63d54b0423bf8aaabb0c99c7c7dba4c548"
+  version "10.1.0"
+  sha256 arm:   "4e109b8900c4127b8bb92c463f98a33a44650b328f3003424adff941fe071dcb",
+         intel: "ed14c3279c8d10b67d3c850abf90ec300a38f3f5666bb67aba4d9c953411ae27"
 
   url "https://github.com/peazip/PeaZip/releases/download/#{version}/peazip-#{version}.DARWIN.#{arch}.dmg",
       verified: "github.com/peazip/PeaZip/"
