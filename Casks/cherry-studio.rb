1055f0d995551fa1ad58507faeb03feba2915c01
ymfsing <ymfsing@outlook.com>
2025-01-09 13:00:42 +0800

    update cherry-studio version to 0.9.5

--- Casks/cherry-studio.rb
+++ Casks/cherry-studio.rb
@@ -1,7 +1,7 @@
 cask "cherry-studio" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.9.4"
+  version "0.9.5"
   sha256 :no_check
   # sha256 arm:   "cef7244f33824ed0693c04d2158bdf9e6c0db5d0ba34084b3061945412dda60c",
   #        intel: "5989f7990f57e6d77bd9b896f978cb73cb71e253e0795e7c2b60691de7548956"
