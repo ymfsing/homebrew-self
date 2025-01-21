3bda507f3327e7dcf7a13e026d390948d430119b
ymfsing <ymfsing@outlook.com>
2025-01-21 10:14:57 +0800

    update cherry-studio version to 0.9.14

--- Casks/cherry-studio.rb
+++ Casks/cherry-studio.rb
@@ -1,7 +1,7 @@
 cask "cherry-studio" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.9.10"
+  version "0.9.14"
   sha256 :no_check
   # sha256 arm:   "cef7244f33824ed0693c04d2158bdf9e6c0db5d0ba34084b3061945412dda60c",
   #        intel: "5989f7990f57e6d77bd9b896f978cb73cb71e253e0795e7c2b60691de7548956"
@@ -24,10 +24,6 @@ cask "cherry-studio" do
   app "Cherry Studio.app"
 
   # zap trash: [
-  #       "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/is.follow.sfl*",
-  #       "~/Library/Application Support/Follow",
   #       "~/Library/Logs/Follow",
-  #       "~/Library/Preferences/is.follow.plist",
-  #       "~/Library/Saved Application State/is.follow.savedState",
   #     ]
 end
