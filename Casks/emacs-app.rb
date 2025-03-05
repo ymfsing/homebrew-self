b2cb5c6f1426be733686060e8c56a27681f5ddc3
ymfsing <ymfsing@outlook.com>
2025-03-05 13:01:11 +0800

    add cask emacs-app

---
+++ Casks/emacs-app.rb
@@ -0,0 +1,70 @@
+cask 'emacs-app' do
+  name 'Emacs'
+  desc 'GNU Emacs text editor'
+  homepage 'https://github.com/jimeh/emacs-builds'
+
+  version '2025-03-01.1e84a87.master'
+
+  on_arm do
+    sha256 'be3ad5120555b3e12ccd8ca904e106c0916808c3cb9e8caab6ef5ea984dcf973'
+    url 'https://github.com/jimeh/emacs-builds/releases/download/Emacs.#{version}/Emacs.#{version}.macOS-11.arm64.dmg'
+    depends_on macos: '>= :big_sur'
+  end
+  on_intel do
+    sha256 'a67e1e5e4883cbf304ecb43197557a1ecf8a68e648bee9618d5c1406c683a54e'
+    url 'https://github.com/jimeh/emacs-builds/releases/download/Emacs.#{version}/Emacs.#{version}.macOS-11.x86_64.dmg'
+    depends_on macos: '>= :big_sur'
+  end
+
+  livecheck do
+    url 'https://github.com/jimeh/emacs-builds.git'
+    strategy :git do |tags|
+      tags.map do |tag|
+        m = /^Emacs\.(\d{4}-\d{2}-01\.\w+\.master)$/.match(tag)
+        next unless m
+
+        m[1]
+      end.compact
+    end
+  end
+
+  conflicts_with(
+    cask: %w[
+      emacs-app
+      emacs-app-good
+      emacs-app-nightly-28
+      emacs-app-nightly-29
+      emacs-app-pretest
+      emacs
+      emacs-nightly
+      emacs-pretest
+      emacs-mac
+      emacs-mac-spacemacs-icon
+    ],
+    formula: %w[
+      emacs
+      emacs-mac
+    ]
+  )
+
+  app 'Emacs.app'
+  binary "#{appdir}/Emacs.app/Contents/MacOS/bin/ebrowse"
+  binary "#{appdir}/Emacs.app/Contents/MacOS/bin/emacs"
+  binary "#{appdir}/Emacs.app/Contents/MacOS/bin/emacsclient"
+  binary "#{appdir}/Emacs.app/Contents/MacOS/bin/etags"
+  binary "#{appdir}/Emacs.app/Contents/Resources/include/emacs-module.h",
+         target: "#{HOMEBREW_PREFIX}/include/emacs-module.h"
+  binary "#{appdir}/Emacs.app/Contents/Resources/site-lisp/subdirs.el",
+         target: "#{HOMEBREW_PREFIX}/share/emacs/site-lisp/subdirs.el"
+
+  manpage "#{appdir}/Emacs.app/Contents/Resources/man/man1/ebrowse.1.gz"
+  manpage "#{appdir}/Emacs.app/Contents/Resources/man/man1/emacs.1.gz"
+  manpage "#{appdir}/Emacs.app/Contents/Resources/man/man1/emacsclient.1.gz"
+  manpage "#{appdir}/Emacs.app/Contents/Resources/man/man1/etags.1.gz"
+
+  zap trash: [
+    '~/Library/Caches/org.gnu.Emacs',
+    '~/Library/Preferences/org.gnu.Emacs.plist',
+    '~/Library/Saved Application State/org.gnu.Emacs.savedState'
+  ]
+end
