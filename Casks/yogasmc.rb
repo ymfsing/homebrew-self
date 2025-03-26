b460b708de48c41644868c1b44d7d020b2005dd8
ymfsing <ymfsing@outlook.com>
2025-03-26 11:49:40 +0800

    add yogasmc

---
+++ Casks/yogasmc.rb
@@ -0,0 +1,20 @@
+cask "yogasmc" do
+  version "1.5.3"
+  sha256 :no_check
+
+  url "https://github.com/zhen-zen/YogaSMC/releases/download/#{version}/YogaSMC-App-Release.dmg"
+  name "YogaSMC"
+  desc "ACPI driver for OEM hardware."
+  homepage "https://github.com/zhen-zen/YogaSMC"
+
+  # auto_updates true
+
+  app "YogaSMC.app"
+
+  # zap trash: [
+  #       "~/Library/Caches/com.seewo.easinote5.mac",
+  #       "~/Library/Caches/com.seewo.easinote5.mac.Shiplt",
+  #       "~/Library/Preferences/com.seewo.easinote5.mac.plist",
+  #       "~/Library/Saved Application State/com.seewo.easinote5.mac.savedState",
+  #     ]
+end
