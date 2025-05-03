f55a1ace9c91f621d59496e72d58b328a4359170
ymfsing <ymfsing@outlook.com>
2025-05-03 14:51:19 +0800

    add cask simple-live

---
+++ Casks/simple-live.rb
@@ -0,0 +1,11 @@
+cask "simple-live" do
+
+  version "1.8.3"
+  sha256 :no_check
+
+  url "https://github.com/xiaoyaocz/dart_simple_live/releases/download/v1.8.3/simple_live_app-1.8.3+10803-macos.dmg"
+  name "Simple Live"
+  desc "简简单单的看直播"
+  homepage "https://github.com/xiaoyaocz/dart_simple_live"
+
+end
