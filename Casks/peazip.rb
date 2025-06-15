e6d16be02d620c641c6cef10cf872b0ba920cdde
ymfsing <ymfsing@outlook.com>
2025-06-16 07:10:53 +0800

    update peazip version

--- Casks/peazip.rb
+++ Casks/peazip.rb
@@ -1,9 +1,9 @@
 cask "peazip" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "10.4.0"
-  sha256 arm:   "546686c6f592ffa65bcedd4b47e76487e9ec980301ca4d4f0d9cae02929ee6e0",
-         intel: "0a7058c9cabc4be43809a55092c3f1f5fc3bc4bc2a3e7c275ed21a25a0c6d556"
+  version "10.5.0"
+  sha256 arm:   "fa120c735680c3ae9ebbd3e1ddf0933eab53dea20ff5862a543154efea397ecf",
+         intel: "49e365c7e71dc74e27d97f5bb73a2312a9b97ee4278e1032fe7f4f4080a9cdc3"
 
   url "https://github.com/peazip/PeaZip/releases/download/#{version}/peazip-#{version}.DARWIN.#{arch}.dmg",
       verified: "github.com/peazip/PeaZip/"
