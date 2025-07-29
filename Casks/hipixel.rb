311c6251dd295e6f438f080e395862de051695a8
ymfsing <ymfsing@outlook.com>
2025-07-29 20:17:59 +0800

    update hipixel

--- Casks/hipixel.rb
+++ Casks/hipixel.rb
@@ -3,7 +3,7 @@ cask "hipixel" do
   version "v0.3.1"
   sha256 :no_check
 
-  url "https://github.com/okooo5km/HiPixel/releases/download/#{version}.0/HiPixel-#{version}.dmg"
+  url "https://github.com/okooo5km/HiPixel/releases/download/#{version}/HiPixel-#{version}.dmg"
   name "HiPixel"
   desc "HiPixel is a native macOS application for AI-powered image super-resolution, built with SwiftUI and leveraging Upscayl's powerful AI models."
   homepage "https://github.com/okooo5km/HiPixel"
