cd3f5dc55c4b3b5c9fe224d332f922360614ae91
ymfsing <ymfsing@outlook.com>
2025-11-30 13:51:56 +0800

    add cask cc-switch

---
+++ Casks/cc-switch.rb
@@ -0,0 +1,30 @@
+cask "cc-switch" do
+
+  # https://github.com/farion1231/homebrew-ccswitch/blob/main/Casks/cc-switch.rb
+
+  version "3.8.0"
+  sha256 "62df8ac322046ffc5c69b8f8ac827fc787da7ddd11bf647ec94b9abe4806625e"
+
+  url "https://github.com/farion1231/cc-switch/releases/download/v#{version}/CC-Switch-v#{version}-macOS.tar.gz"
+  name "CC Switch"
+  desc "Configuration manager for Claude Code and Codex providers"
+  homepage "https://github.com/farion1231/cc-switch"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on macos: ">= :big_sur"
+
+  app "CC Switch.app"
+
+  zap trash: [
+    "~/.cc-switch",
+    "~/Library/Application Support/com.ccswitch.desktop",
+    "~/Library/Caches/com.ccswitch.desktop",
+    "~/Library/Preferences/com.ccswitch.desktop.plist",
+    "~/Library/Saved Application State/com.ccswitch.desktop.savedState",
+    "~/Library/WebKit/com.ccswitch.desktop",
+  ]
+end
