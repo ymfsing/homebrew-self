0a3b57a7060b35871bd2437ce6e99c8e36789930
ymfsing <ymfsing@outlook.com>
2025-12-02 00:17:26 +0800

    update mole version to 1.11.11

--- Formula/mole.rb
+++ Formula/mole.rb
@@ -4,8 +4,8 @@ class Mole < Formula
 
   desc "Comprehensive macOS cleanup and application uninstall tool"
   homepage "https://github.com/tw93/mole"
-  url "https://github.com/tw93/Mole/archive/refs/tags/V1.11.2.tar.gz"
-  sha256 "11a4b8e1fab0ff81011c2a4a4e96f45307bdb23dabbd6a9296ccd3c40501e1bf"
+  url "https://github.com/tw93/Mole/archive/refs/tags/V1.11.11.tar.gz"
+  sha256 "244329b3e5076d85330de6c74a344ba4082f4b46b476a82fd69ccb773c6dec01"
   license "MIT"
   head "https://github.com/tw93/mole.git", branch: "main"
 
