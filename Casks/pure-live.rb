0feab6b6284af43450eb9daa7f49aa735f455222
ymfsing <ymfsing@outlook.com>
2026-01-03 00:03:09 +0800

    update

--- Casks/pure-live.rb
+++ Casks/pure-live.rb
@@ -3,7 +3,7 @@ cask "pure-live" do
   version "2.0.11"
   sha256 :no_check
 
-  url "https://github.com/iuchuancong/pure_live/releases/download/v#{version}/PureLive-#{version}+22-macOS.dmg"
+  url "https://github.com/liuchuancong/pure_live/releases/download/v#{version}/PureLive-#{version}+22-macOS.dmg"
   name "Pure Live"
   desc "第三方直播播放器，支持多个主流直播平台的视频流播放。"
   homepage "https://github.com/liuchuancong/pure_live"
