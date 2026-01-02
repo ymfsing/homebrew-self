7baa131321d82075079ab3ff4398e031553e56a9
ymfsing <ymfsing@outlook.com>
2026-01-03 00:00:21 +0800

    delete mole and update purelive

--- Casks/pure-live.rb
+++ Casks/pure-live.rb
@@ -1,12 +1,12 @@
 cask "pure-live" do
 
-  version "2.1.0"
+  version "2.0.11"
   sha256 :no_check
 
-  url "https://github.com/aydomini/Pure-Live/releases/download/v#{version}/PureLive-macOS-#{version}.dmg"
+  url "https://github.com/iuchuancong/pure_live/releases/download/v#{version}/PureLive-#{version}+22-macOS.dmg"
   name "Pure Live"
   desc "第三方直播播放器，支持多个主流直播平台的视频流播放。"
-  homepage "https://github.com/aydomini/Pure-Live"
+  homepage "https://github.com/liuchuancong/pure_live"
 
   app "Pure-Live.app"
 
