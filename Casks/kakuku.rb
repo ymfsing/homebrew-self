9e2590d9a3454d42dbdb8784ba2106e27ce1e5ed
ymfsing <ymfsing@outlook.com>
2026-03-10 21:01:54 +0800

    add cask kaku

---
+++ Casks/kakuku.rb
@@ -0,0 +1,27 @@
+cask "kakuku" do
+
+  # https://github.com/tw93/homebrew-tap/blob/main/Casks/kakuku.rb
+ 
+  version "0.6.0"
+  sha256 "bf691254e9e280a512e4233445157350ad734cedcd1b50d015d9a6d5c68d7f07"
+
+  url "https://github.com/tw93/Kaku/releases/download/V#{version}/Kaku.dmg",
+      verified: "github.com/tw93/Kaku/"
+  name "Kaku"
+  desc "A fast, out-of-the-box terminal built for AI coding"
+  homepage "https://github.com/tw93/Kaku"
+
+  conflicts_with cask: "kaku"
+
+  auto_updates true
+
+  app "Kaku.app"
+  binary "#{appdir}/Kaku.app/Contents/MacOS/kaku", target: "kaku"
+
+  zap trash: [
+    "~/Library/Application Support/kaku",
+    "~/Library/Caches/kaku",
+    "~/Library/Preferences/fun.tw93.kaku.plist",
+    "~/Library/Saved Application State/fun.tw93.kaku.savedState",
+  ]
+end
