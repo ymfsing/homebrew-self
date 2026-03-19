49f734ac3367fafd776b6769d7e6b8acc209edf0
ymfsing <ymfsing@outlook.com>
2026-03-19 21:47:44 +0800

    update hipixel version to V0.4.3

--- Casks/hipixel.rb
+++ Casks/hipixel.rb
@@ -1,19 +1,28 @@
 cask "hipixel" do
 
-  version "v0.4.1"
-  sha256 :no_check
+  # https://github.com/okooo5km/homebrew-tap/blob/main/Casks/hipixel.rb
 
-  url "https://github.com/okooo5km/HiPixel/releases/download/#{version}/HiPixel-#{version}.dmg"
+  version "0.4.3"
+  sha256 "f8f21bc0b50d646a16ef3ac4dd5cd7454f734170dec0fee92b1125c05ce24a8d"
+
+  url "https://releases.5km.tech/hipixel/HiPixel-v#{version}.dmg"
   name "HiPixel"
-  desc "HiPixel is a native macOS application for AI-powered image super-resolution, built with SwiftUI and leveraging Upscayl's powerful AI models."
-  homepage "https://github.com/okooo5km/HiPixel"
+  desc "Image upscaling tool that makes images crystal clear"
+  homepage "https://hipixel.5km.tech/"
+
+  livecheck do
+    url "https://releases.5km.tech/hipixel/appcast.xml"
+    strategy :sparkle, &:short_version
+  end
+
+  auto_updates true
+  depends_on macos: ">= :ventura"
 
   app "HiPixel.app"
 
-  # zap trash: [
-  #       "~/Library/Caches/com.seewo.easinote5.mac",
-  #       "~/Library/Caches/com.seewo.easinote5.mac.Shiplt",
-  #       "~/Library/Preferences/com.seewo.easinote5.mac.plist",
-  #       "~/Library/Saved Application State/com.seewo.easinote5.mac.savedState",
-  #     ]
+  zap trash: [
+    "~/Library/Application Support/tech.5km.HiPixel",
+    "~/Library/Preferences/tech.5km.HiPixel.plist",
+    "~/Library/Saved Application State/tech.5km.HiPixel.savedState",
+  ]
 end
