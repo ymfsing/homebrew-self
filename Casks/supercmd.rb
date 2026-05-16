41418da5dda44af562d28d7fdad01bc5c4eda6e1
ymfsing <ymfsing@outlook.com>
2026-05-16 19:42:01 +0800

    update supercmd version to 1.0.23

--- Casks/supercmd.rb
+++ Casks/supercmd.rb
@@ -1,7 +1,7 @@
 cask "supercmd" do
 
-  version "1.0.22"
-  sha256 "8a034268501db16ac130853505d6d82c9e2f07e6cd929bb316d8fe3085f38f86"
+  version "1.0.23"
+  sha256 "f86975e05c7fce4f967d46c21de8e0bcb5b6f249892541584571a6585d125fa5"
 
   url "https://github.com/SuperCmdLabs/SuperCmd/releases/download/#{version}/SuperCmd-#{version}.dmg"
   name "SuperCmd"
