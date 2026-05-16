8c07f8eee5d6d2ffdc6865f0db1f22c1dce0fb81
ymfsing <ymfsing@outlook.com>
2026-05-16 19:38:03 +0800

    update cc-switch version to 3.15.0

--- Casks/cc-switch.rb
+++ Casks/cc-switch.rb
@@ -2,8 +2,8 @@ cask "cc-switch" do
 
   # https://github.com/farion1231/homebrew-ccswitch/blob/main/Casks/cc-switch.rb
 
-  version "3.14.1"
-  sha256 "c27774f9d67546807752b753df4c907ef77aceabf0a04ea2e46c396ac49de16b"
+  version "3.15.0"
+  sha256 "2b53643202c66eb84f3b1f3604c6486e6a8bcb9b4b3312be9f5d6a349f658019"
 
   url "https://github.com/farion1231/cc-switch/releases/download/v#{version}/CC-Switch-v#{version}-macOS.tar.gz"
   name "CC Switch"
