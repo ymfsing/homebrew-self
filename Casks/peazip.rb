2e41e6f5e00dfc71815de513cac4fe6233541fa5
ymfsing <ymfsing@outlook.com>
2026-09-16 00:24:04 +0800

    update

--- Casks/peazip.rb
+++ Casks/peazip.rb
@@ -5,8 +5,7 @@ cask "peazip" do
   sha256 arm:   "8e354e950935b4cfbd25da71a80d18ad125ab204483ad41dd8dbb097d8f32a8e",
          intel: "54c1df5c8384eb1fc1494b8c13df10b7474b191a7d989a5aa088a565cd96fa21"
 
-  url "https://github.com/peazip/PeaZip/releases/download/#{version}/peazip-#{version}.DARWIN.#{arch}.dmg",
-      verified: "github.com/peazip/PeaZip/"
+  url "https://github.com/peazip/PeaZip/releases/download/#{version}/peazip-#{version}.DARWIN.#{arch}.dmg"
   name "PeaZip"
   desc "Open source archive manager"
   homepage "https://peazip.github.io/"
